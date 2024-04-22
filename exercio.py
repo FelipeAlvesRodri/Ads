32ce4e599678e40f0d010be46cb6e7acb06b5d9c
FelipeAlvesRodri <00001106743088sp@al.educacao.sp.gov.br>
2024-04-22 08:49:56 -0300

    Verificar
    Verifica se seu numero é positivo ou negativo

---
+++ exercio.py
@@ -0,0 +1,11 @@
+numero = int(input("Digite um numero: "))
+
+if numero > 0:
+    print("seu numero é positivo")
+
+if numero ==0:
+ print("seu numero é nulo")
+
+if numero < 0:
+   print("seu numero é negativo")
+
