c0b6a61d5afd801520b8186580259cbea357558a
FelipeAlvesRodri <161459772+FelipeAlvesRodri@users.noreply.github.com>
2024-05-07 14:13:50 -0300

    Create alfabeto.py
    jogo de stop

---
+++ .github/workflows/alfabeto.py
@@ -0,0 +1,57 @@
+letra = input("Digite uma letra do Alfabeto ")
+letra = letra.lower()
+
+if letra =='a':
+    print("Amanda")
+elif letra =='b':
+    print("Bruna")
+elif letra =='c':
+    print("Carolina")
+elif letra =='d': 
+    print("Daniela")
+elif letra =='e':
+    print("Eliza")
+elif letra =='f':
+    print("Felipe")
+elif letra =='g':
+    print("Gabriella")
+elif letra =='h':
+    print("Helena")
+elif letra =='i':
+    print("Irene")
+elif letra =='j':
+    print("João")
+elif letra =='k':
+    print("Keila")
+elif letra =='l':
+    print("Laura")
+elif letra =='m':
+    print("Manuella")
+elif letra =='n':
+    print("Natalia")
+elif letra =='o':
+    print("Ofelia")
+elif letra =='p':
+    print("Paola")
+elif letra =='q':
+    print("Quel")
+elif letra =='r':
+    print("Renata")
+elif letra =='s':
+    print("Sarah")
+elif letra =='t':
+    print("Tania")
+elif letra =='u':
+    print("Ursula")
+elif letra =='v':
+    print("Vanessa")
+elif letra =='x':
+    print("Xuxa")
+elif letra =='z':
+    print("Zelda")
+elif letra =='w':
+    print("Wilda")
+elif letra =='y':
+    print("Yara")
+else:
+    print("Letra invalida!")
