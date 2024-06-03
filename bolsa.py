d535c35ae7280ca09fb24d64f6602a658e575391
FelipeAlvesRodri <161459772+FelipeAlvesRodri@users.noreply.github.com>
2024-06-03 20:56:52 -0300

    Delete bolsa.py

--- bolsa.py
+++
@@ -1,9 +0,0 @@
-gpa = float(input("Digite sua pontução: "))
-top_10 = int(input("Qual foi seu rank da sua turma: "))
-trabalho = int(input("Quantas horas vc trabalhou voluntariamente: "))
-               
-if gpa >= 3.5 or top_10 <= 10 or trabalho > 100 :
-    print("Vc é legivel para a bolsa!")
-else: 
-    print("vc n é legivel para bolsa!")
-
