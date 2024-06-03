522c8f8118aef17e44ed443e1040ad2a61d7a3ad
FelipeAlvesRodri <161459772+FelipeAlvesRodri@users.noreply.github.com>
2024-06-03 20:51:43 -0300

    Delete qtd_minima = 10.py

--- qtd_minima = 10.py
+++
@@ -1,8 +0,0 @@
-qtd_minima = 10
-qtd_produto = int(input("Quantidade do produto: "))
-
-if qtd_produto > qtd_minima:
-    print("Produto disponivel em estoque. Aproveite!")
-else:
-    print("Produto indisponivel no estoque. Volte mais tarde!")
-    
